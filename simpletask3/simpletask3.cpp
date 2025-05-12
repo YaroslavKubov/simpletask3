25a5dd472f201c77bd9130bf6a1eaf9bfcdba633
YaroslavKubov <yaroslavkubov@gmail.com>
2025-05-13 02:56:24 +0300

    Добавьте файлы проекта.

---
+++ simpletask3/simpletask3.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    
+    std::cout << "enter a:";
+    std::cin >> a;
+
+    std::cout << "enter b:";
+    std::cin >> b;
+
+    int sum = a + b;
+
+    int diff = a - b; 
+
+    int pr = a * b;
+
+    std::cout << "sum = " << sum << std::endl;
+    std::cout << "difference = " << diff << std::endl;
+    std::cout << "product = " << pr;
+
+
+    return 0;
+}
